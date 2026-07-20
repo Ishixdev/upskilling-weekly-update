9a2f53892683574e528c061289e1489ecc1631ff
tejaswini <tejaswini.chilakamarry@gmail.com>
2026-07-20 12:29:06 +0530

    Added Exercise 7 - Financial Forecasting

---
+++ Week-1/Algorithms_ds/src/exercise7_forecasting/Main.java
@@ -0,0 +1,17 @@
+package exercise7_forecasting;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double currentValue = 1000;
+        double growthRate = 0.10;
+        int years = 3;
+
+        double futureValue = FinancialForecast.predictFutureValue(currentValue, growthRate, years);
+
+        System.out.println("Future Value: " + futureValue);
+    }
+}
